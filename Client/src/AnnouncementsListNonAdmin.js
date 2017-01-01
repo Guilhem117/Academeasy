018fe69f25ee4940c72933107cd8b1484f537c14
Amira Zighmi <shazadiamira@gmail.com>
2017-01-01 15:07:47 +0100

    Affichage d'un message quand il n'y a pas d'annonce

--- Client/src/AnnouncementsListNonAdmin.js
+++ Client/src/AnnouncementsListNonAdmin.js
@@ -4,42 +4,47 @@ import {Grid, Panel, ListGroup, ListGroupItem} from 'react-bootstrap';
 import AnnouncementsStore from './Stores/Announcements';
 
 class AnnouncementsListNonAdmin extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          announcements: [],
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      announcements: []
+    };
 
-    }
+  }
 
-    componentWillMount() {
-      AnnouncementsStore.getAnnouncements().then((announcements) => {
-        this.setState({announcements});
-      });
-    }
+  componentWillMount() {
+    AnnouncementsStore.getAnnouncements().then((announcements) => {
+      this.setState({announcements});
+    });
+  }
 
-    componentWillReceiveProps() {
-      AnnouncementsStore.getAnnouncements().then((announcements) => {
-        this.setState({announcements});
-      });
-    }
+  componentWillReceiveProps() {
+    AnnouncementsStore.getAnnouncements().then((announcements) => {
+      this.setState({announcements});
+    });
+  }
 
+  render() {
+    return (
+      <Grid className="table-background">
+        <Panel header="Announcements">
+          {(this.state.announcements && this.state.announcements.length > 0)
+            ? <ListGroup>
+                {this.state.announcements.map((announcement) => {
+                  const {courses} = announcement;
+                  return (
+                    <ListGroupItem key={announcement.id}>
+                      <strong>{courses.join(', ')}</strong>: {announcement.text}
+                    </ListGroupItem>
+                  );
+                })}
+              </ListGroup>
+            : <h3>No Announcement</h3>}
+        </Panel>
+      </Grid>
 
-    render() {
-        return (
-          <Grid className="table-background">
-              <Panel header="Announcements">
-                <ListGroup>
-                  {this.state.announcements.map((announcement) => {
-                    const {courses} = announcement;
-                    return <ListGroupItem key={announcement.id}><strong>{courses.join(', ')} :</strong> {announcement.text}</ListGroupItem>
-                  })}
-                </ListGroup>
-              </Panel>
-          </Grid>
-
-        );
-    }
+    );
+  }
 }
 
 export default AnnouncementsListNonAdmin;
