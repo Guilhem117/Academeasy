2e3201b29db1cc4817f5977c87781b1a202c0ff8
Amira Zighmi <shazadiamira@gmail.com>
2017-01-11 12:25:18 +0100

    Ajout une page 404

---
+++ Client/src/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Grid, Row} from 'react-bootstrap';
+
+export default(props) => {
+  return (
+    <Grid>
+      <Row className="text-center">
+        <h1>Page not found</h1>
+      </Row>
+    </Grid>
+  );
+}
--- Client/src/index.js
+++ Client/src/index.js
@@ -19,6 +19,8 @@ import AdminsList from './AdminsList';
 import StudentsScan from './StudentsScan';
 import App from './App';
 
+import NotFound from './404';
+
 import UsersStore from './Stores/Users';
 
 import {ReactAlert} from './Alerts';
@@ -30,89 +32,90 @@ import './index.css';
 import './TableManagement.css';
 
 function redirectToLogin(nextState, replace, callback) {
-    UsersStore.getSession().then((session) => {
-        let {role, username} = session;
-        if (role && username) {
-            sessionStorage.role = role;
-            sessionStorage.username = username;
-        } else {
-            return Promise.reject();
-        }
-    }).then(_ => {
-        callback();
-    }).catch(_ => {
-        replace('/login');
-        callback();
-    });
+  UsersStore.getSession().then((session) => {
+    let {role, username} = session;
+    if (role && username) {
+      sessionStorage.role = role;
+      sessionStorage.username = username;
+    } else {
+      return Promise.reject();
+    }
+  }).then(_ => {
+    callback();
+  }).catch(_ => {
+    replace('/login');
+    callback();
+  });
 }
 
 ReactDOM.render(
-    <Router history={hashHistory}>
-    <Route path="/" component={App} onEnter={redirectToLogin}>
-        <IndexRedirect to="home"/>
-        <Route path="home" components={{
-            navBar: NavBar,
-            content: MainPage
-        }}/>
-        <Route path="courses" components={{
-            navBar: NavBar,
-            content: CoursesList
-        }}/>
-        <Route path="course/:courseCode" components={{
-            navBar: NavBar,
-            content: CourseDetails
-        }}/>
-        <Route path="students" components={{
-            navBar: NavBar,
-            content: StudentsList
-        }}/>
-        <Route path="student/:studentId" components={{
-            navBar: NavBar,
-            content: StudentDetails
-        }}/>
-        <Route path="teachers" components={{
-            navBar: NavBar,
-            content: TeachersList
-        }}/>
-        <Route path="teacher/:teacherId" components={{
-            navBar: NavBar,
-            content: TeacherDetails
-        }}/>
-        <Route path="profile" components={{
-            navBar: NavBar,
-            content: ProfileDetails
-        }}/>
-        <Route path="calendar" components={{
-            navBar: NavBar,
-            content: Timetable
-        }}/>
-        <Route path="calendar/:eventId" components={{
-            navBar: NavBar,
-            content: Timetable
-        }}/>
-        <Route path="announcements" components={{
-            navBar: NavBar,
-            content: AnnouncementsList
-        }}/>
-        <Route path="announcement/:announceId" components={{
-            navBar: NavBar,
-            content: AnnouncementDetails
-        }}/>
-        <Route path="scan" components={{
-            navBar: NavBar,
-            content: StudentsScan
-        }}/>
-        <Route path="scan/:eventId" components={{
-            navBar: NavBar,
-            content: StudentsScan
-        }}/>
-        <Route path="admin" components={{
-            navBar: NavBar,
-            content: AdminsList
-        }}/>
-    </Route>
-    <Route path="/login" component={LoginDialog}/>
+  <Router history={hashHistory}>
+  <Route path="/" component={App} onEnter={redirectToLogin}>
+    <IndexRedirect to="home"/>
+    <Route path="home" components={{
+      navBar: NavBar,
+      content: MainPage
+    }}/>
+    <Route path="courses" components={{
+      navBar: NavBar,
+      content: CoursesList
+    }}/>
+    <Route path="course/:courseCode" components={{
+      navBar: NavBar,
+      content: CourseDetails
+    }}/>
+    <Route path="students" components={{
+      navBar: NavBar,
+      content: StudentsList
+    }}/>
+    <Route path="student/:studentId" components={{
+      navBar: NavBar,
+      content: StudentDetails
+    }}/>
+    <Route path="teachers" components={{
+      navBar: NavBar,
+      content: TeachersList
+    }}/>
+    <Route path="teacher/:teacherId" components={{
+      navBar: NavBar,
+      content: TeacherDetails
+    }}/>
+    <Route path="profile" components={{
+      navBar: NavBar,
+      content: ProfileDetails
+    }}/>
+    <Route path="calendar" components={{
+      navBar: NavBar,
+      content: Timetable
+    }}/>
+    <Route path="calendar/:eventId" components={{
+      navBar: NavBar,
+      content: Timetable
+    }}/>
+    <Route path="announcements" components={{
+      navBar: NavBar,
+      content: AnnouncementsList
+    }}/>
+    <Route path="announcement/:announceId" components={{
+      navBar: NavBar,
+      content: AnnouncementDetails
+    }}/>
+    <Route path="scan" components={{
+      navBar: NavBar,
+      content: StudentsScan
+    }}/>
+    <Route path="scan/:eventId" components={{
+      navBar: NavBar,
+      content: StudentsScan
+    }}/>
+    <Route path="admin" components={{
+      navBar: NavBar,
+      content: AdminsList
+    }}/>
+  </Route>
+  <Route path="/login" component={LoginDialog}/>
+  <Route path="*" component={NotFound}/>
 </Router>, document.getElementById('root'));
 
 ReactDOM.render(
-    <ReactAlert className="toast-top-right"/>, document.getElementById('toastr'));
+  <ReactAlert className="toast-top-right"/>, document.getElementById('toastr'));
