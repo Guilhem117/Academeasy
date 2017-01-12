2ec3afe7d4e6c336a0be330f23d185685178f696
Amira Zighmi <shazadiamira@gmail.com>
2017-01-12 21:56:50 +0100

    Pas de print du chat

--- Client/src/ChatReact.js
+++ Client/src/ChatReact.js
@@ -330,7 +330,7 @@ class ChatReact extends Component {
     </div>;
 
     return (
-      <div className="chat no-print">
+      <div className="chat hidden-print">
         {this.state.isConnected && <Panel header={chatBar} bsStyle="primary" style={{
           width: '300px'
         }} collapsible expanded={this.state.isVisible}>
